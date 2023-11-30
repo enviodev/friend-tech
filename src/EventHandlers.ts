8b54422bceafb51f086bfab24c30382e4e7be773
moose-code <jonjonclark@gmail.com>
2023-11-30 16:59:44 +0000

    Added the subject and stats

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -13,6 +13,7 @@ import {
   TradeEntity,
   EventsSummaryEntity,
   TraderEntity,
+  SubjectEntity,
 } from "./src/Types.gen";
 
 const GLOBAL_EVENTS_SUMMARY_KEY = "GlobalEventsSummary";
@@ -23,6 +24,7 @@ const INITIAL_EVENTS_SUMMARY: EventsSummaryEntity = {
   tradesCount: BigInt(0),
   totalBuys: BigInt(0),
   totalSells: BigInt(0),
+  totalProtocolEthAmount: BigInt(0),
 };
 
 const INITIAL_TRADER: TraderEntity = {
@@ -33,6 +35,17 @@ const INITIAL_TRADER: TraderEntity = {
   totalBuys: BigInt(0),
   totalSells: BigInt(0),
   provisionalProfitOrLoss: BigInt(0),
+};
+
+const INITIAL_SUBJECT: SubjectEntity = {
+  id: "0x00000",
+  numberOfTrades: BigInt(0),
+  totalBuys: BigInt(0),
+  totalSells: BigInt(0),
+  totalShareBuys: BigInt(0),
+  totalShareSells: BigInt(0),
+  supply: BigInt(0),
+  totalSubjectEthAmount: BigInt(0),
 };
 
 FriendtechSharesV1Contract_OwnershipTransferred_loader(({ event, context }) => {
@@ -67,6 +80,7 @@ FriendtechSharesV1Contract_OwnershipTransferred_handler(
 FriendtechSharesV1Contract_Trade_loader(({ event, context }) => {
   context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
   context.Trader.load(event.params.trader.toString());
+  context.Subject.load(event.params.subject.toString());
 });
 
 FriendtechSharesV1Contract_Trade_handler(({ event, context }) => {
@@ -78,12 +92,14 @@ FriendtechSharesV1Contract_Trade_handler(({ event, context }) => {
   let nextSummaryEntity = {
     ...currentSummaryEntity,
     tradesCount: currentSummaryEntity.tradesCount + BigInt(1),
-    totalBuys: event.params.isBuy
-      ? currentSummaryEntity.totalBuys + BigInt(1)
-      : currentSummaryEntity.totalBuys,
-    totalSells: !event.params.isBuy
-      ? currentSummaryEntity.totalSells + BigInt(1)
-      : currentSummaryEntity.totalSells,
+    totalProtocolEthAmount:
+      currentSummaryEntity.totalProtocolEthAmount +
+      event.params.protocolEthAmount,
+    totalBuys: incrementIf(event.params.isBuy, currentSummaryEntity.totalBuys),
+    totalSells: incrementIf(
+      !event.params.isBuy,
+      currentSummaryEntity.totalSells
+    ),
   };
 
   let tradeEntity: TradeEntity = {
@@ -108,13 +124,9 @@ FriendtechSharesV1Contract_Trade_handler(({ event, context }) => {
   let nextTraderEntity = {
     ...TraderEntity,
     id: event.params.trader.toString(),
-    numberOfTrades: TraderEntity.numberOfTrades + BigInt(1),
-    totalBuys: event.params.isBuy
-      ? TraderEntity.totalBuys + BigInt(1)
-      : TraderEntity.totalBuys,
-    totalSells: !event.params.isBuy
-      ? TraderEntity.totalSells + BigInt(1)
-      : TraderEntity.totalSells,
+    numberOfTrades: TraderEntity.numberOfTrades + 1n,
+    totalBuys: incrementIf(event.params.isBuy, TraderEntity.totalBuys),
+    totalSells: incrementIf(!event.params.isBuy, TraderEntity.totalSells),
     ethAmountFromBuys: event.params.isBuy
       ? TraderEntity.ethAmountFromBuys + event.params.ethAmount
       : TraderEntity.ethAmountFromBuys,
@@ -124,8 +136,38 @@ FriendtechSharesV1Contract_Trade_handler(({ event, context }) => {
     provisionalProfitOrLoss: event.params.isBuy
       ? TraderEntity.provisionalProfitOrLoss - event.params.ethAmount
       : TraderEntity.provisionalProfitOrLoss +
-        (event.params.ethAmount * BigInt(9)) / BigInt(10), // take 10% fee into account.
+        event.params.ethAmount -
+        event.params.protocolEthAmount -
+        event.params.subjectEthAmount, // take fee cut into account.
   };
 
   context.Trader.set(nextTraderEntity);
+
+  let subject = context.Subject.get(event.params.subject.toString());
+  let SubjectEntity: SubjectEntity = subject ?? INITIAL_SUBJECT;
+
+  let nextSubjectEntity = {
+    ...SubjectEntity,
+    id: event.params.subject.toString(),
+    numberOfTrades: SubjectEntity.numberOfTrades + 1n,
+    totalBuys: incrementIf(event.params.isBuy, SubjectEntity.totalBuys),
+    totalSells: incrementIf(!event.params.isBuy, SubjectEntity.totalSells),
+    totalShareBuys: event.params.isBuy
+      ? SubjectEntity.totalShareBuys + event.params.shareAmount
+      : SubjectEntity.totalShareBuys,
+    totalShareSells: !event.params.isBuy
+      ? SubjectEntity.totalShareSells + event.params.shareAmount
+      : SubjectEntity.totalShareSells,
+    supply: event.params.isBuy
+      ? SubjectEntity.supply + event.params.shareAmount
+      : SubjectEntity.supply - event.params.shareAmount,
+    totalSubjectEthAmount:
+      SubjectEntity.totalSubjectEthAmount + event.params.subjectEthAmount,
+  };
+
+  context.Subject.set(nextSubjectEntity);
 });
+
+function incrementIf(condition: boolean, value: bigint): bigint {
+  return condition ? value + 1n : value;
+}
